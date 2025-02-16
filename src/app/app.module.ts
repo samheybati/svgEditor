23a29e9a0d29ce24466d346fdc9d8e878b718a05
Samin <samin.heybati.dev@gmail.com>
2025-02-16 20:30:17 +0330

    feat(app-component):create project

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {RouterModule, RouterOutlet} from '@angular/router';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([])
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
